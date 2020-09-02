de64629a603fbdde61ae7c50ae95d4dce309a5b1
David Chen <david.chen@samarkand.global>
2020-09-02 15:34:54 +0100

    note: try to fix error: ValueError: At least two variables have the same name: embedding/embeddings_IL/Adam

--- model/AbstractRecommender.py
+++ model/AbstractRecommender.py
@@ -175,6 +175,10 @@ class AbstractRecommender(object):
         return cache__sessions[self.cache_key]
 
     def save_tf_model(self):
+        # note: try to fix error below
+        # ValueError: At least two variables have the same name: embedding/embeddings_IL/Adam  # noqa:E501
+        tf.reset_default_graph()
+
         saver = tf.train.Saver()
         # TODO add checkpoints
         saver.save(self.sess, self.tf_cache_path, save_debug_info=True)
